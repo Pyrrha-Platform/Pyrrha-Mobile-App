87241ecb32a896a286c66bef71d5a43fa3f2cdf1
mrodrise <mrodrise@gmail.com>
2020-08-13 20:18:21 +0200

    Initial version

---
+++ app/src/main/java/com/prometeo/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.prometeo.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Change this part");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
